f1edbe2075d456cd27a1be42bb8cc93148dc5c29
Sayed Mehedi <>
2022-09-18 19:28:18 +0600

    confirm parchas add

--- src/Screen/AddShippingAddressScreen.tsx
+++ src/Screen/AddShippingAddressScreen.tsx
@@ -4,18 +4,240 @@ import {useAppSelector} from "@hooks/store";
 import {RootStackParamList} from "@src/types";
 import {RootStackRoutes} from "@constants/routes";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {Controller, useFieldArray, useForm} from "react-hook-form";
+import {ErrorMessage} from "@hookform/error-message";
+import {useTheme,} from "react-native-paper";
+import AppPrimaryButton from '../Component/AppPrimaryButton'
+import Feather from 'react-native-vector-icons/Feather'
+import {
+  setGlobalStyles,
+  FloatingLabelInput,
+} from "react-native-floating-label-input";
+
+
+setGlobalStyles.containerStyles = {
+  height: 58,
+  borderRadius: 6,
+  paddingHorizontal: 10,
+  backgroundColor: "#fff",
+};
+
+setGlobalStyles.customLabelStyles = {
+  fontSizeFocused: 12,
+  fontSizeBlurred: 15,
+  colorFocused: "#707070",
+  colorBlurred: "#707070",
+};
+
+setGlobalStyles.labelStyles = {
+  paddingTop: 5,
+  paddingHorizontal: 5,
+  fontFamily: "Inter-Regular",
+};
+
+setGlobalStyles.inputStyles = {
+  fontSize: 15,
+  marginTop: 15,
+  color: "#707070",
+  fontWeight: "600",
+};
 
 type Props = NativeStackScreenProps<
   RootStackParamList,
   typeof RootStackRoutes.ADD_SHIPPING_ADDRESS
 >;
+type FormValues = {
+  buynowprice: "";
+  beginDay: Date;
+  quantity: number;
+  isListNow: boolean;
+  startingPrice: "";
+  showMetalPrice: boolean;
+};
 
 const AddShippingAddressScreen = ({navigation, route}: Props) => {
+  const theme = useTheme();
+  
+  const {
+    reset,
+    control,
+    setError,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      name: "",
+      adress: "",
+      state:"",
+      city:"",
+      zipCode:"",
+      phone:""
+    },
+  });
   const profile = useAppSelector(state => state.auth.profile);
 
   return (
-    <View>
-      <Text>AddShippingAddressScreen</Text>
+    <View style={{padding:20}}>
+       <View style={{width: "100%", marginBottom: 16}}>
+                <Controller
+                  control={control}
+                  name={"name"}
+                  render={({field}) => {
+                    return (
+                      <FloatingLabelInput
+                        label={"Name*"}
+                        value={field.value}
+                        onChangeText={field.onChange}
+                      />
+                    );
+                  }}
+                />
+
+                <ErrorMessage
+                  errors={errors}
+                  name={"name"}
+                  render={({message}) => (
+                    <Text style={{color: theme.colors.error, marginTop: 10}}>
+                      {message}
+                    </Text>
+                  )}
+                />
+              </View>
+
+              <View style={{width: "100%", marginBottom: 16}}>
+                <Controller
+                  control={control}
+                  name={"adress"}
+                  render={({field}) => {
+                    return (
+                      <FloatingLabelInput
+                        label={"Address*"}
+                        value={field.value}
+                        onChangeText={field.onChange}
+                      />
+                    );
+                  }}
+                />
+
+                <ErrorMessage
+                  errors={errors}
+                  name={"adress"}
+                  render={({message}) => (
+                    <Text style={{color: theme.colors.error, marginTop: 10}}>
+                      {message}
+                    </Text>
+                  )}
+                />
+              </View>
+
+              <View style={{flexDirection:'row',justifyContent:'space-between',marginBottom:16}}>
+              <View style={{
+                height:60,
+                width:'48%',
+                backgroundColor:'#FFFFFF',padding:10,
+                borderRadius:8
+              }}>
+                <Text style={{
+                  fontFamily:'Inter-Regular',
+                  fontSize:12,
+                  color:'#111111'
+                }}>State*</Text>
+                <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+                  <Text style={{
+                    fontFamily:'Inter-Regular',
+                    fontSize:14,
+                    color:'#707070'
+                  }}>Select</Text>
+                  <Feather name="chevron-down" size={25} color={'#707070'}/>
+                </View>
+              </View>
+
+              <View style={{
+                height:60,
+                width:'48%',
+                backgroundColor:'#FFFFFF',padding:10,
+                borderRadius:8
+              }}>
+                <Text style={{
+                  fontFamily:'Inter-Regular',
+                  fontSize:12,
+                  color:'#111111'
+                }}>City*</Text>
+                <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+                  <Text style={{
+                    fontFamily:'Inter-Regular',
+                    fontSize:14,
+                    color:'#707070'
+                  }}>Select</Text>
+                  <Feather name="chevron-down" size={25} color={'#707070'}/>
+                </View>
+              </View>
+              </View>
+
+              <View style={{width: "100%", marginBottom: 16}}>
+                <Controller
+                  control={control}
+                  name={"zipCode"}
+                  render={({field}) => {
+                    return (
+                      <FloatingLabelInput
+                        label={"ZipCode*"}
+                        value={field.value}
+                        onChangeText={field.onChange}
+                      />
+                    );
+                  }}
+                />
+
+                <ErrorMessage
+                  errors={errors}
+                  name={"zipCode"}
+                  render={({message}) => (
+                    <Text style={{color: theme.colors.error, marginTop: 10}}>
+                      {message}
+                    </Text>
+                  )}
+                />
+              </View>
+
+              <View style={{width: "100%", marginBottom: 16}}>
+                <Controller
+                  control={control}
+                  name={"phone"}
+                  render={({field}) => {
+                    return (
+                      <FloatingLabelInput
+                        label={"Mobile Number*"}
+                        value={field.value}
+                        onChangeText={field.onChange}
+                      />
+                    );
+                  }}
+                />
+
+                <ErrorMessage
+                  errors={errors}
+                  name={"phone"}
+                  render={({message}) => (
+                    <Text style={{color: theme.colors.error, marginTop: 10}}>
+                      {message}
+                    </Text>
+                  )}
+                />
+              </View>
+
+              <AppPrimaryButton
+            text={"Save"}
+            containerStyle={{
+              alignSelf: "center",
+            }}
+            onPress={()=>{}}
+            disabled={false}
+          />
+
+              
+
+             
     </View>
   );
 };
--- src/Screen/ConfirmPurchaseScreen.tsx
+++ src/Screen/ConfirmPurchaseScreen.tsx
@@ -1,21 +1,274 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, Text, TouchableOpacity, Image} from "react-native";
 import {useAppSelector} from "@hooks/store";
 import {RootStackParamList} from "@src/types";
 import {RootStackRoutes} from "@constants/routes";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {Controller, useFieldArray, useForm} from "react-hook-form";
+import {CheckBox} from "react-native-elements";
+import AppPrimaryButton from '../Component/AppPrimaryButton'
 
 type Props = NativeStackScreenProps<
   RootStackParamList,
   typeof RootStackRoutes.CONFIRM_PURCHASE
 >;
+type FormValues = {
+  buynowprice: "";
+  beginDay: Date;
+  quantity: number;
+  isListNow: boolean;
+  startingPrice: "";
+  showMetalPrice: boolean;
+};
+
+
 
 const ConfirmPurchaseScreen = ({navigation, route}: Props) => {
+  const {control, handleSubmit} = useForm<FormValues>({
+    defaultValues: {},
+  });
   const profile = useAppSelector(state => state.auth.profile);
+  const productImage = route.params.productImage;
+  const productName = route.params.productName;
+  
 
   return (
-    <View>
-      <Text>ConfirmPurchaseScreen</Text>
+    <View style={{padding: 10}}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          paddingVertical: 10,
+        }}>
+        <Image
+          source={{uri: productImage}}
+          style={{height: 80, width: 80, borderRadius: 5}}
+        />
+
+        <Text
+          style={{
+            fontFamily: "Inter-Bold",
+            fontSize: 14,
+            color: "#111111",
+            marginLeft: 8,
+          }}>
+          {productName}
+        </Text>
+      </View>
+
+      <View
+        style={{
+          borderTopWidth: 1,
+          borderColor: "#D6D6D6",
+          borderBottomWidth: 1,
+          paddingVertical: 15,
+        }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}>
+          <Text style={{
+            fontSize: 16,
+            fontFamily: "Inter-SemiBold",
+            color: "#111111",
+          }}>Shipping</Text>
+          <TouchableOpacity
+          onPress={()=>navigation.navigate(RootStackRoutes.ADD_SHIPPING_ADDRESS)}
+          >
+            <Text style={{
+               fontFamily: "Inter-Regular",
+               fontSize: 12,
+               color: "#023047",
+            }}>Add</Text>
+          </TouchableOpacity>
+        </View>
+        <Text
+          style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 12,
+            color: "#667085",
+          }}>
+          Please add your shipping address
+        </Text>
+      </View>
+
+      <Text
+        style={{
+          marginTop: 10,
+          fontSize: 16,
+          fontFamily: "Inter-SemiBold",
+          color: "#111111",
+        }}>
+        Payment Method
+      </Text>
+      <View
+        style={{
+          marginTop: 10,
+          marginLeft: -10,
+          flexDirection: "row",
+          alignItems: "center",
+        }}>
+        <Controller
+          control={control}
+          name={"isListNow"}
+          render={({field}) => {
+            return (
+              <CheckBox
+                center
+                checked={field.value}
+                uncheckedIcon={"circle-o"}
+                checkedIcon={"dot-circle-o"}
+                containerStyle={{
+                  padding: 1,
+                  paddingLeft: 0,
+                }}
+                onPress={() => field.onChange(true)}
+              />
+            );
+          }}
+        />
+
+        <Text>Cryptocurrency(BTC/LTC/BCH/ETH/XRP)</Text>
+      </View>
+
+      <View
+        style={{
+          marginTop: 10,
+          marginLeft: -10,
+          flexDirection: "row",
+          alignItems: "center",
+        }}>
+        <Controller
+          control={control}
+          name={"isListNow"}
+          render={({field}) => {
+            return (
+              <CheckBox
+                center
+                checked={field.value}
+                uncheckedIcon={"circle-o"}
+                checkedIcon={"dot-circle-o"}
+                containerStyle={{
+                  padding: 1,
+                  paddingLeft: 0,
+                }}
+                onPress={() => field.onChange(true)}
+              />
+            );
+          }}
+        />
+
+        <Text>Paypal</Text>
+      </View>
+
+      <View
+        style={{
+          marginTop: 10,
+          marginLeft: -10,
+          flexDirection: "row",
+          alignItems: "center",
+        }}>
+        <Controller
+          control={control}
+          name={"isListNow"}
+          render={({field}) => {
+            return (
+              <CheckBox
+                center
+                checked={field.value}
+                uncheckedIcon={"circle-o"}
+                checkedIcon={"dot-circle-o"}
+                containerStyle={{
+                  padding: 1,
+                  paddingLeft: 0,
+                }}
+                onPress={() => field.onChange(true)}
+              />
+            );
+          }}
+        />
+
+        <Text>Credit card(Stripe)</Text>
+      </View>
+
+      <View
+      style={{
+        height:1,
+        width:'100%',
+        backgroundColor:'#D6D6D6',
+        marginVertical:20
+      }}
+      />
+      <Text style={{
+        fontSize: 16,
+        fontFamily: "Inter-SemiBold",
+        color: "#111111",
+      }}>Order Info</Text>
+      <View style={{marginTop:10}}>
+        <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:5}}>
+          <Text style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 12,
+            color: "#111111",
+          }}>Product Price</Text>
+          <Text style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 12,
+            color: "#111111",
+          }}>$15.00</Text>
+        </View>
+
+        <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+          <Text style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 12,
+            color: "#111111",
+          }}>Shipping</Text>
+          <Text style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 12,
+            color: "#111111",
+
+          }}>$7.00</Text>
+        </View>
+
+        <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:5}}>
+          <Text style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 12,
+            color: "#111111",
+          }}>Tax</Text>
+          <Text style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 12,
+            color: "#111111",
+          }}>$0.00</Text>
+        </View>
+
+        <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:5}}>
+          <Text style={{
+            fontFamily:'Inter-Bold',
+            fontSize:12,
+            color:'#111111'
+          }}>Total</Text>
+          <Text style={{
+            fontFamily:'Inter-Bold',
+            fontSize:12,
+            color:'#111111'
+          }}>$22.99</Text>
+        </View>
+      </View>
+
+      <AppPrimaryButton
+            text={"Confirm Offer"}
+            containerStyle={{
+              alignSelf: "center",
+            }}
+            onPress={()=>{}}
+            disabled={false}
+          />
     </View>
   );
 };
