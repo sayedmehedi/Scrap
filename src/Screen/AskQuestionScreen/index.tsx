32019ad69bebb10688bcbc65b36a7bf057911e01
Joyant-Bhakto <joybd0101@gmail.com>
2022-09-26 04:02:01 +0600

    [msg]: fixed ask question screen scroll issue

--- src/Screen/AskQuestionScreen/index.tsx
+++ src/Screen/AskQuestionScreen/index.tsx
@@ -9,6 +9,7 @@ import {Text, Title, useTheme} from "react-native-paper";
 import AppPrimaryButton from "../../Component/AppPrimaryButton";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {useCreateAskQuestionMutation} from "@data/laravel/services/question";
+import {ScrollView} from "react-native-gesture-handler";
 
 type Props = NativeStackScreenProps<
   RootStackParamList,
@@ -69,69 +70,57 @@ const AskQuestionScreen = ({route, navigation}: Props) => {
       });
   };
 
-  const ListHeaderComponent = React.useMemo(() => {
-    return (
-      <React.Fragment>
-        <View
-          style={{
-            padding: 15,
-            alignItems: "center",
-            flexDirection: "row",
-            borderRadius: theme.roundness * 2,
-            backgroundColor: theme.colors.white,
-          }}>
-          <View>
-            <Image
-              source={{uri: route.params.productImage}}
-              style={{height: 67, width: 67, borderRadius: theme.roundness}}
-            />
-          </View>
-
-          <View style={{flex: 1, paddingHorizontal: 15}}>
-            <Text style={{fontWeight: "700", fontSize: 18}}>
-              {route.params.productName}
-            </Text>
-          </View>
-
-          <View>
-            <Avatar
-              rounded
-              size={"large"}
-              source={{uri: route.params.sellerImage}}
-            />
-
-            <Text style={{fontSize: 13, marginTop: 5}}>
-              {route.params.sellerName}
-            </Text>
-          </View>
+  return (
+    <ScrollView style={{flex: 1}}>
+      <View
+        style={{
+          padding: 15,
+          alignItems: "center",
+          flexDirection: "row",
+          borderRadius: theme.roundness * 2,
+          backgroundColor: theme.colors.white,
+        }}>
+        <View>
+          <Image
+            source={{uri: route.params.productImage}}
+            style={{height: 67, width: 67, borderRadius: theme.roundness}}
+          />
         </View>
 
-        <Title
-          style={{
-            maxWidth: "70%",
-            textAlign: "center",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginVertical: 25,
-          }}>
-          Click a message to send or write your own question
-        </Title>
-      </React.Fragment>
-    );
-  }, [
-    route.params.productImage,
-    route.params.productName,
-    route.params.sellerImage,
-    route.params.sellerName,
-  ]);
+        <View style={{flex: 1, paddingHorizontal: 15}}>
+          <Text style={{fontWeight: "700", fontSize: 18}}>
+            {route.params.productName}
+          </Text>
+        </View>
 
-  return (
-    <View style={{flex: 1}}>
+        <View>
+          <Avatar
+            rounded
+            size={"large"}
+            source={{uri: route.params.sellerImage}}
+          />
+
+          <Text style={{fontSize: 13, marginTop: 5}}>
+            {route.params.sellerName}
+          </Text>
+        </View>
+      </View>
+
+      <Title
+        style={{
+          maxWidth: "70%",
+          textAlign: "center",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginVertical: 25,
+        }}>
+        Click a message to send or write your own question
+      </Title>
+
       <View style={{flex: 1}}>
         <QuestionList
           sellerId={+route.params.sellerId}
           productId={+route.params.productId}
-          ListHeaderComponent={ListHeaderComponent}
         />
       </View>
 
@@ -159,7 +148,7 @@ const AskQuestionScreen = ({route, navigation}: Props) => {
           />
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
