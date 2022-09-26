98adc5a3c66e5b8670f0b0308b8bdbc049e01e74
Joyant-Bhakto <joybd0101@gmail.com>
2022-09-26 19:16:56 +0600

    [msg]: fixed seller review flatlist issue with keyboard

--- src/Screen/ReviewScreen/SellerReviewScreen.tsx
+++ src/Screen/ReviewScreen/SellerReviewScreen.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import dayjs from "dayjs";
 import useAppSnackbar from "@hooks/useAppSnackbar";
 import {RootStackRoutes} from "@constants/routes";
-import {View, Text, TextInput} from "react-native";
 import {FlatList} from "react-native-gesture-handler";
 import AppPrimaryButton from "@src/Component/AppPrimaryButton";
+import {View, Text, TextInput, ListRenderItem, StyleSheet} from "react-native";
 import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 import {Card, Paragraph, Title, useTheme} from "react-native-paper";
 import {Avatar, LinearProgress, Rating} from "react-native-elements";
@@ -13,7 +13,6 @@ import {
   GetSellerReviewsResponse,
   PaginationQueryParams,
   RootStackParamList,
-  SellerReview,
 } from "@src/types";
 import {
   useCreateSellerReviewMutation,
@@ -78,7 +77,7 @@ export default function SellerReviewScreen({route}: Props) {
     }
   }, [isSuccess, data, enqueueSuccessSnackbar]);
 
-  const handleSendReview = () => {
+  const handleSendReview = React.useCallback(() => {
     createReview({
       rating: myRating,
       review: myReview,
@@ -89,9 +88,9 @@ export default function SellerReviewScreen({route}: Props) {
         setMyRating(0);
         setMyReview("");
       });
-  };
+  }, [createReview, route.params.sellerId, myRating, myReview]);
 
-  const getNextReviews = async () => {
+  const getNextReviews = React.useCallback(async () => {
     if (isFetchingNextPage || isFetchingInitial) {
       return;
     }
@@ -121,7 +120,12 @@ export default function SellerReviewScreen({route}: Props) {
       });
     } finally {
     }
-  };
+  }, [
+    isFetchingNextPage,
+    isFetchingInitial,
+    sellerReviewPages,
+    getSellerReviews,
+  ]);
 
   React.useEffect(() => {
     return () => {
@@ -153,141 +157,199 @@ export default function SellerReviewScreen({route}: Props) {
       reviewPage.data.map(review => ({
         type: "data" as const,
         ...review,
+        created_at: dayjs(review.created_at, "DD MMMM YYYY").fromNow(),
       })),
     );
   }, [isLoading, sellerReviewPages]);
 
-  return (
-    <FlatList<typeof reviews[0]>
-      contentContainerStyle={{
-        padding: 10,
-      }}
-      onEndReached={getNextReviews}
-      ListHeaderComponent={() => (
-        <React.Fragment>
-          <Title style={{fontWeight: "700"}}>Review Summary</Title>
+  const ListHeaderComponent = React.useMemo(() => {
+    return (
+      <React.Fragment>
+        <Title style={{fontWeight: "700"}}>Review Summary</Title>
 
-          <Card style={{marginVertical: 20}}>
-            <Card.Content>
-              {reviewSummaries.map(summary => (
-                <View
-                  key={summary.id}
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    marginBottom: 15,
-                  }}>
-                  <View style={{marginRight: 10}}>
-                    <Rating
-                      imageSize={15}
-                      readonly={true}
-                      showRating={false}
-                      startingValue={summary.rating}
-                    />
-                  </View>
-
-                  <View style={{marginRight: 10}}>
-                    <Text>({summary.rating})</Text>
-                  </View>
-
-                  <View style={{flex: 1, marginRight: 10}}>
-                    <LinearProgress
-                      color={"#FFB703"}
-                      variant={"determinate"}
-                      value={summary.progress()}
-                    />
-                  </View>
-
-                  <View>
-                    <Text>{summary.percentage()}%</Text>
-                  </View>
-                </View>
-              ))}
-            </Card.Content>
-          </Card>
-
-          <Title style={{fontWeight: "700"}}>Your Rating</Title>
-
-          <Card style={{marginVertical: 20}}>
-            <Card.Content>
-              <Rating
-                showRating={false}
-                startingValue={myRating}
-                onFinishRating={setMyRating}
-              />
-
-              <View style={{marginTop: 20}}>
-                <TextInput
-                  multiline
-                  value={myReview}
-                  numberOfLines={4}
-                  textAlignVertical={"top"}
-                  onChangeText={setMyReview}
-                  placeholder="Write review"
-                  style={{
-                    borderWidth: 1,
-                    borderColor: "#191F2B",
-                    paddingHorizontal: 20,
-                    borderRadius: theme.roundness * 5,
-                  }}
-                />
-              </View>
-            </Card.Content>
-            <Card.Actions
-              style={{justifyContent: "center", paddingVertical: 30}}>
-              <AppPrimaryButton
-                onPress={handleSendReview}
-                disabled={!myReview || isCreatingReview}
-                text="Submit"
-              />
-            </Card.Actions>
-          </Card>
-        </React.Fragment>
-      )}
-      data={reviews}
-      ItemSeparatorComponent={() => <View style={{height: 15}} />}
-      renderItem={({item}) => {
-        if (item.type === "skeleton") {
-          return (
-            <SkeletonPlaceholder>
-              <SkeletonPlaceholder.Item paddingBottom={15}>
-                <SkeletonPlaceholder.Item height={100} borderRadius={5} />
-              </SkeletonPlaceholder.Item>
-            </SkeletonPlaceholder>
-          );
-        }
-
-        return (
-          <Card>
-            <Card.Title
-              title={item.user_name}
-              subtitle={dayjs(item.created_at, "DD MMMM YYYY").fromNow()}
-              left={() => (
-                <Avatar
-                  rounded
-                  size={"small"}
-                  source={{uri: item.user_image}}
-                />
-              )}
-              right={() => (
-                <View style={{paddingRight: 10}}>
+        <Card style={{marginVertical: 20}}>
+          <Card.Content>
+            {reviewSummaries.map(summary => (
+              <View
+                key={summary.id}
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  marginBottom: 15,
+                }}>
+                <View style={{marginRight: 10}}>
                   <Rating
                     imageSize={15}
                     readonly={true}
                     showRating={false}
-                    startingValue={item.rating}
+                    startingValue={summary.rating}
                   />
                 </View>
-              )}
+
+                <View style={{marginRight: 10}}>
+                  <Text>({summary.rating})</Text>
+                </View>
+
+                <View style={{flex: 1, marginRight: 10}}>
+                  <LinearProgress
+                    color={"#FFB703"}
+                    variant={"determinate"}
+                    value={summary.progress()}
+                  />
+                </View>
+
+                <View>
+                  <Text>{summary.percentage()}%</Text>
+                </View>
+              </View>
+            ))}
+          </Card.Content>
+        </Card>
+
+        <Title style={{fontWeight: "700"}}>Your Rating</Title>
+
+        <Card style={{marginVertical: 20}}>
+          <Card.Content>
+            <Rating
+              showRating={false}
+              startingValue={myRating}
+              onFinishRating={setMyRating}
             />
 
-            <Card.Content>
-              <Paragraph>{item.review}</Paragraph>
-            </Card.Content>
-          </Card>
-        );
-      }}
+            <View style={{marginTop: 20}}>
+              <TextInput
+                multiline
+                value={myReview}
+                numberOfLines={4}
+                textAlignVertical={"top"}
+                onChangeText={setMyReview}
+                placeholder="Write review"
+                style={{
+                  borderWidth: 1,
+                  borderColor: "#191F2B",
+                  paddingHorizontal: 20,
+                  borderRadius: theme.roundness * 5,
+                }}
+              />
+            </View>
+          </Card.Content>
+          <Card.Actions style={{justifyContent: "center", paddingVertical: 30}}>
+            <AppPrimaryButton
+              text="Submit"
+              onPress={handleSendReview}
+              disabled={!myReview || isCreatingReview}
+            />
+          </Card.Actions>
+        </Card>
+      </React.Fragment>
+    );
+  }, [reviewSummaries, handleSendReview, myReview, isCreatingReview]);
+
+  return (
+    <MemoizedReviewList
+      reviews={reviews}
+      getNextReviews={getNextReviews}
+      ListHeaderComponent={ListHeaderComponent}
     />
   );
+}
+
+const renderEachReviewItem: ListRenderItem<
+  | {
+      id: number;
+      rating: number;
+      review: string;
+      user_name: string;
+      created_at: string;
+      user_image: string;
+      type: "data";
+    }
+  | {
+      id: number;
+      type: "skeleton";
+    }
+> = ({item}) => {
+  if (item.type === "skeleton") {
+    return (
+      <SkeletonPlaceholder>
+        <SkeletonPlaceholder.Item paddingBottom={15}>
+          <SkeletonPlaceholder.Item height={100} borderRadius={5} />
+        </SkeletonPlaceholder.Item>
+      </SkeletonPlaceholder>
+    );
+  }
+
+  return (
+    <Card>
+      <Card.Title
+        title={item.user_name}
+        subtitle={item.created_at}
+        left={() => (
+          <Avatar rounded size={"small"} source={{uri: item.user_image}} />
+        )}
+        right={() => (
+          <View style={{paddingRight: 10}}>
+            <Rating
+              imageSize={15}
+              readonly={true}
+              showRating={false}
+              startingValue={item.rating}
+            />
+          </View>
+        )}
+      />
+
+      <Card.Content>
+        <Paragraph>{item.review}</Paragraph>
+      </Card.Content>
+    </Card>
+  );
+};
+
+function ReviewList({
+  reviews,
+  getNextReviews,
+  ListHeaderComponent,
+}: {
+  reviews: Array<
+    | {
+        id: number;
+        rating: number;
+        review: string;
+        user_name: string;
+        created_at: string;
+        user_image: string;
+        type: "data";
+      }
+    | {
+        id: number;
+        type: "skeleton";
+      }
+  >;
+  getNextReviews: () => void;
+  ListHeaderComponent: JSX.Element;
+}) {
+  return (
+    <FlatList<typeof reviews[0]>
+      data={reviews}
+      onEndReached={getNextReviews}
+      renderItem={renderEachReviewItem}
+      ListHeaderComponent={ListHeaderComponent}
+      ItemSeparatorComponent={ItemSeparatorComponent}
+      contentContainerStyle={flatlistStyles.contentContainerStyle}
+    />
+  );
+}
+
+const flatlistStyles = StyleSheet.create({
+  contentContainerStyle: {
+    padding: 10,
+  },
+});
+
+const MemoizedReviewList = React.memo(ReviewList);
+
+function ItemSeparatorComponent() {
+  return <View style={{height: 15}} />;
 }
